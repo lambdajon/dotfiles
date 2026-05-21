09e44548981b4770230439c9287849fca1e74e33
lambdajon <lambdajon42@gmail.com>
2026-05-21 13:14:29 +0500

    remove hyperland pkg

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -1,8 +1,7 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     xwayland.enable = true;
   };
 
