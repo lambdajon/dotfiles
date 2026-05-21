095b7c417ead8d627664387544bf17b3ac90dabb
lambdajon <lambdajon42@gmail.com>
2026-05-21 20:09:03 +0500

    zen

---
+++ modules/home/browsers/zen.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  programs.zen-browser = {
+    enable = true;
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
+}
