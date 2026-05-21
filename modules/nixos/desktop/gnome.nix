2f0d197df00374b11a46c81a1524d9c1781bdce2
lambdajon <lambdajon42@gmail.com>
2026-05-21 13:57:26 +0500

    maybe fix ?

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -15,68 +15,68 @@
       };
 
       excludePackages = [ pkgs.xterm ];
+    };
 
-      desktopManager.gnome = {
-        enable = true;
+    desktopManager.gnome = {
+      enable = true;
 
-        extraGSettingsOverrides = ''
-          [org.gnome.desktop.background]
-          picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}'
+      extraGSettingsOverrides = ''
+        [org.gnome.desktop.background]
+        picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}'
 
-          [org.gnome.desktop.background]
-          picture-uri-dark='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
+        [org.gnome.desktop.background]
+        picture-uri-dark='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
 
-          [org.gnome.desktop.interface]
-          color-scheme='prefer-dark'
+        [org.gnome.desktop.interface]
+        color-scheme='prefer-dark'
 
-          [org.gnome.shell]
-          favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.SystemMonitor.desktop', 'org.gnome.Console.desktop', 'org.gnome.gitg.desktop', 'org.gnome.Builder.desktop', 'org.gnome.Polari.desktop']
+        [org.gnome.shell]
+        favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.SystemMonitor.desktop', 'org.gnome.Console.desktop', 'org.gnome.gitg.desktop', 'org.gnome.Builder.desktop', 'org.gnome.Polari.desktop']
 
-          [org.gnome.shell]
-          disable-user-extensions=false
+        [org.gnome.shell]
+        disable-user-extensions=false
 
-          [org.gnome.shell]
-          enabled-extensions=['user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'light-style@gnome-shell-extensions.gcampax.github.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com']
+        [org.gnome.shell]
+        enabled-extensions=['user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'light-style@gnome-shell-extensions.gcampax.github.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com']
 
-          [org.gnome.mutter]
-          dynamic-workspaces=true
+        [org.gnome.mutter]
+        dynamic-workspaces=true
 
-          [org.gnome.mutter]
-          edge-tiling=true
+        [org.gnome.mutter]
+        edge-tiling=true
 
-          [org.gnome.desktop.interface]
-          icon-theme='Papirus-Dark'
+        [org.gnome.desktop.interface]
+        icon-theme='Papirus-Dark'
 
-          [org.gnome.desktop.datetime]
-          automatic-timezone=true
+        [org.gnome.desktop.datetime]
+        automatic-timezone=true
 
-          [org.gnome.tweaks]
-          show-extensions-notice=false
+        [org.gnome.tweaks]
+        show-extensions-notice=false
 
-          [org.gnome.desktop.wm.preferences]
-          button-layout='appmenu:minimize,maximize,close'
+        [org.gnome.desktop.wm.preferences]
+        button-layout='appmenu:minimize,maximize,close'
 
-          [org.gnome.desktop.interface]
-          monospace-font-name='JetBrainsMono Nerd Font 10'
+        [org.gnome.desktop.interface]
+        monospace-font-name='JetBrainsMono Nerd Font 10'
 
-          [org.gnome.shell.extensions.dash-to-dock]
-          multi-monitor=true
+        [org.gnome.shell.extensions.dash-to-dock]
+        multi-monitor=true
 
-          [org.gnome.shell.extensions.dash-to-dock]
-          apply-custom-theme=true
+        [org.gnome.shell.extensions.dash-to-dock]
+        apply-custom-theme=true
 
-          [org.gnome.settings-daemon.plugins.power]
-          sleep-inactive-ac-type='nothing'
+        [org.gnome.settings-daemon.plugins.power]
+        sleep-inactive-ac-type='nothing'
 
-          [org.gnome.desktop.session]
-          idle-delay=0
-        '';
+        [org.gnome.desktop.session]
+        idle-delay=0
+      '';
 
-        extraGSettingsOverridePackages = [
-          pkgs.gsettings-desktop-schemas
-          pkgs.gnome-shell
-        ];
-      };
+      extraGSettingsOverridePackages = [
+        pkgs.gsettings-desktop-schemas
+        pkgs.gnome-shell
+      ];
     };
 
     udev.packages = [ pkgs.gnome-settings-daemon ];
