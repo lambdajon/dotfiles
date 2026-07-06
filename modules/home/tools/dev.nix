ea93105ea933fe9125f70349c55a01f6cd8f9f91
lambdajon <lambdajon42@gmail.com>
2026-07-06 06:21:40 +0500

    rust and haskell specific configs for fast building

---
+++ modules/home/tools/dev.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    sccache
+    clang
+  ];
+
+  home.sessionVariables = {
+    RUSTC_WRAPPER = "sccache";
+    SCCACHE_CACHE_SIZE = "10G";
+    GHCRTS = "-N";
+  };
+
+  home.file.".cargo/config.toml".text = ''
+    [target.x86_64-unknown-linux-gnu]
+    linker = "clang"
+    rustflags = ["-C", "link-arg=-fuse-ld=mold", "-C", "target-cpu=x86-64-v3"]
+  '';
+
+  home.file.".cabal/config".text = ''
+    jobs: $ncpusx
+  '';
+}
